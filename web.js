db354ee8642f420032f7757b892b9b7009887ac6
Ubuntu <ubuntu@ip-172-31-21-40.us-west-2.compute.internal>
2013-07-10 14:17:43 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,15 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-//var fs = require('fs');
+var fs = require('fs');
 
-//var file = 'index.html';
+var file = 'index.html';
 
 //var buffer = fs.readFileSync('index.html', 'utf-8');
 
 //var message = 'here';
 
-/*fs.exists(file, function(exists) {
+fs.exists(file, function(exists) {
   if(exists) {
     fs.stat(file, function(error, stats) {
       fs.open(file, 'r', function(error, fd) {
@@ -31,9 +31,9 @@ var app = express.createServer(express.logger());
     }
 });*/
 
-app.get('/', function(request, response) {
-  response.send('Hello world!');
-});
+//app.get('/', function(request, response) {
+//  response.send('Hello world!');
+//});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
